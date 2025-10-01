d42b86d1447daea06b865ef0597fd8b8cacc1e2e
DERVISEMREDEMIR-STUD <g00427391@atu.ie>
2025-10-01 15:03:10 +0100

    @NotBlank for name, @NotNull, @Positive is added for price for validity of the object.

--- src/main/java/ie/atu/week3lab2/Product.java
+++ src/main/java/ie/atu/week3lab2/Product.java
@@ -1,12 +1,21 @@
 package ie.atu.week3lab2;
 
+import com.sun.jdi.DoubleValue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class Product {
+
+    @NotBlank(message = "Name can not be blank!")
     private String productName;
+
+    @NotNull(message = "Price must not be null!")
+    @Positive(message = "Price must be positive!")
     private double price;
 
 }
